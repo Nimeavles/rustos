cff60e011222a9d8f04e3c41b1d23e377d372d14
Nimeavles <prlf505@gmail.com>
2023-08-27 19:41:57 +0200

    baremetal hello world display

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 #![no_std]
 #![no_main]
-#![no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Buffer to display on screen
+static HELLO: &[u8] = b"Hello World";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Cast 0xb8000 into a raw pointer; 0xb8000 is the address
+    // where vga buffer is located
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x6;
+        }
+    }
+
     loop {}
 }

e1450cc4a014881e4c58854ce8a6984410bde57c
Nimeavles <prlf505@gmail.com>
2023-08-28 12:39:27 +0200

    vga buffer safe driver

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,16 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
 
-// Buffer to display on screen
-static HELLO: &[u8] = b"Hello World";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Cast 0xb8000 into a raw pointer; 0xb8000 is the address
-    // where vga buffer is located
-
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x6;
-        }
-    }
-
+    println!("Hello World {}", "!");
     loop {}
 }
